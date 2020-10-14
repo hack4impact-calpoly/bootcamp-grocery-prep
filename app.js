7a7266fb280395ac6d1974597f1b89a0efac3a8b
Mike Preis <mikepreis@users.noreply.github.com>
2020-10-14 04:37:06 -0700

    add servings button

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("App.js loaded");
+
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+    document.getElementById("myText").value = "Johnny Bravo";
+  });
