20784227bfd1349c1fb31812bfb36e19191b012e
Ben McMann <bbmcmann@volosys.com>
2020-10-15 18:29:58 -0600

    implemented counting on Almond page

--- servings.js
+++ servings.js
@@ -1,14 +1,26 @@
-const ingredients = document.getElementsByClassName("ingredients");
+const ingredients = document.getElementsByClassName("counter");
 
 document.addEventListener('click', event => {
-    if (event.target.id === "ingredients")
-        add1(1);
+    if (event.target.id === "up")
+        changeServings(1);
+    if (event.target.id === "down")
+        changeServings(-1);
     
 });
 
-const add1 = (direction) => {
-    const currentServings = document.getElementById("count");
+const changeServings = (direction) => {
+    const currentServings = document.getElementById("serving-count");
     const servingCount = Number(currentServings.textContent);
+    const newServings = servingCount + direction;
 
-    currentServings.textContent = servingCount + direction;
+    if(newServings < 1) return;
+
+    currentServings.textContent = newServings;
+
+    Array.prototype.map.call(ingredients, e => {
+
+        const itemServing = Number(e.getAttribute('ammount'));
+        e.textContent = +(itemServing * newServings).toFixed(2);
+
+    })
 }
