b28e4f131d8b3e426b38d4e61159c24ad5cf2a63
konanyuta <58442094+konanyuta@users.noreply.github.com>
2020-10-17 15:15:26 -0700

    moved files into folders; added button for servings

---
+++ recipe.js
@@ -0,0 +1,22 @@
+const countElements = document.getElementsByClassName('amount');
+
+document.addEventListener('click', event => {
+  if (event.target.id === 'sub')
+    updateServings(-1);
+  if (event.target.id === 'add')
+    updateServings(1);
+});
+
+const updateServings = (direction) => {
+  const currentServings = document.getElementById('count');
+  const servingCount = Number(currentServings.textContent);
+  const newServings = servingCount + direction;
+
+  if (newServings < 1) return;
+  currentServings.textContent = newServings;
+
+  Array.prototype.map.call(countElements, e => {
+    const itemServing = Number(e.getAttribute('base'));
+    e.textContent = +(itemServing * newServings).toFixed(2);
+  })
+}
