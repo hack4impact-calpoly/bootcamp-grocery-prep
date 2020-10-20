7ebca94f3136353a6ad2d7ea435002cbc575400c
esauerbo <esauerbo@calpoly.edu>
2020-10-20 15:07:55 -0700

    made buttons to change serving sizes and blurred background images for redability

--- ingredients.js
+++ ingredients.js
@@ -1,22 +1,21 @@
-const countElements = document.getElementsByClassName('count');
+const countElements = document.getElementsByClassName('count')
 
 document.addEventListener('click', event => {
-  if (event.target.id === 'subtract')
-    updateServings(-1);
-  if (event.target.id === 'add')
-    updateServings(1);
-});
+  if (event.target.id === 'dec')
+    updateServings(-1)
+  if (event.target.id === 'inc')
+    updateServings(1)
+})
 
 const updateServings = (direction) => {
-  const currentServings = document.getElementById('num-servings');
-  const servingCount = Number(currentServings.textContent);
-  const newServings = servingCount + direction;
+  const currentServings = document.getElementById('serving-count')
+  const servingCount = Number(currentServings.textContent)
+  const newServings = servingCount + direction
 
-  if (newServings < 1) return;
-  currentServings.textContent = newServings;
+  if (newServings < 1) return
+  currentServings.textContent = newServings
 
   Array.prototype.map.call(countElements, e => {
-    const itemServing = Number(e.getAttribute('base'));
-    e.textContent = +(itemServing * newServings).toFixed(2);
+    e.textContent = +((e.textContent / servingCount) * newServings).toFixed(2)
   })
 }
