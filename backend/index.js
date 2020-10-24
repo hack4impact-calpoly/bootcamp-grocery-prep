fc6bb8313b0cb1a1748c8c189c4dac306e38539f
Samuel Sehnert <samuel.sehnert@gmail.com>
2020-10-24 14:26:06 -0700

    orgaization

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,14 @@ const express = require("express");
 const app = express();
 
 app.listen(3000);
+app.use(express.static("."))
 
+app.get("/", (req, res) => {
+    res.status(200);
+    res.send("index.html");
+})
+
+app.get("/home", (req, res) => {
+    res.status(200);
+    res.send("hello!!")
+})
