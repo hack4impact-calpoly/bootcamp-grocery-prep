b98ef34bf50366cf8d10c8c58c1d85cc63fa3f13
Cole <ctperry0301@gmail.com>
2020-10-24 16:42:39 -0700

    Week 3 Work

---
+++ GroceryPrep/backend/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send("test")
+})
+
+app.get('/api/recipe', (req, res) => {
+  res.status(200)
+  res.send("list of recipes requested")
+})
+
+app.get('/api/recipe/random', (req, res) => {
+  res.status(200)
+  res.send("Random recipe requested")
+})
+
+app.get('/api/recipe/:name', (req, res) => {
+  const name = req.params.name
+  res.status(200)
+  res.send(`Hello ${name}!`)
+})
+
+app.get('/api/cart', (req, res) => {
+  const name = req.params.name
+  res.status(200)
+  res.send(`Here are your cart items`)
+})
+
+app.post('/api/rating', (req, res) => {
+  const rating = req.body.rating
+  const id = req.body.id
+  res.status(200)
+  res.send(`Rating of ${rating} recieved for ${id}`)
+})
+
+app.post('/api/cart', (req, res) => {
+  const quantity = req.body.quantity
+  const id = req.body.id
+  res.status(200)
+  res.send(`${quantity} orders of recipe ${id} added to cart`)
+})
+
+app.listen(3000)
