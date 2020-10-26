e5ef348cd8246fb774de25c29e2a4d3e9624026d
SageM5862 <SageM5862@gmail.com>
2020-10-25 19:41:59 -0700

    backend stuff

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/api/recipe', (req, res) => {
+  res.status(200)
+  res.send(`list of recipes requested`)
+})
+
+app.get('/api/recipe/random', (req, res) => {
+  res.status(200)
+  res.send(`random recipe requested`)
+})
+
+app.get('/api/recipe/:name', (req, res) => {
+  const name = req.body.name
+  res.status(200)
+  res.send(`instructions for ${name} requested`)
+})
+
+app.post('/api/rating', (req, res) => {
+  const rating = req.body.rating
+  const id = req.body.id
+  res.status(200)
+  res.send(`rating of ${rating} recieved for ${id}`)
+})
+
+
+app.get('/api/cart', (req, res) => {
+  const name = req.params.name
+  res.status(200)
+  res.send(`here are the items in your cart`)
+})
+
+app.post('/api/cart', (req, res) => {
+  const quantity = req.body.quantity
+  const id = req.body.id
+  res.status(200)
+  res.send(`${quantity} orders of recipe ${id} added to cart`)
+})
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
+
