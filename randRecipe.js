8f90687afd7560c2feaaeb98c72a1a71256001c4
Jon <jhbanh@calpoly.edu>
2020-10-27 16:20:02 -0700

    Fixed API Instruct... Need Rating and Ingreds

--- randRecipe.js
+++ randRecipe.js
@@ -3,6 +3,7 @@ const URL = 'https://3blzgwgi13.execute-api.us-west-2.amazonaws.com/Live/recipe'
 const recipe_name = document.getElementById('recipe_name')
 const recipe_desc = document.getElementById('recipe_desc')
 const rand_img = document.getElementById('rand_img')
+const instructList = document.getElementById('instructList')
 
 const getRecipe = async () => {
   // use the fetch API to get data from
@@ -20,6 +21,18 @@ const populateElements = data => {
     rand_img.src = data.picture
     rand_img.height = "400"
     rand_img.width = "600"
+
+    // Ingredients
+
+    // Instructions
+    let instructLength = data.instructions.length
+    for (let x = 0; x < instructLength; x++){
+      let z = document.createElement("LI")
+      let y = document.createTextNode(data.instructions[x])
+      z.appendChild(y)
+      instructList.appendChild(z)
+    }
 }
+
 
 getRecipe()
