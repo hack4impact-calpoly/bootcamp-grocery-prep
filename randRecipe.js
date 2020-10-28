657af96a2fe845f7e1129b7c44b62787119c7662
Jon <jhbanh@calpoly.edu>
2020-10-27 19:55:45 -0700

    Still working on API

--- randRecipe.js
+++ randRecipe.js
@@ -3,6 +3,7 @@ const URL = 'https://3blzgwgi13.execute-api.us-west-2.amazonaws.com/Live/recipe'
 const recipe_name = document.getElementById('recipe_name')
 const recipe_desc = document.getElementById('recipe_desc')
 const rand_img = document.getElementById('rand_img')
+const ingredList = document.getElementById('ingredList')
 const instructList = document.getElementById('instructList')
 
 const getRecipe = async () => {
@@ -11,7 +12,7 @@ const getRecipe = async () => {
   // log it to the console
   fetch(URL)
   .then(response => response.json())
-  //.then(data => console.log(data))
+  // .then(data => console.log(data))
   .then(data => populateElements(data))
 }
 
@@ -23,14 +24,22 @@ const populateElements = data => {
     rand_img.width = "600"
 
     // Ingredients
+    let ingredLength = data.ingredients.length
+    console.log(data.ingredients)
+    for (let x1 = 0; x1 < ingredLength; x1++){
+      let z1 = document.createElement("LI")
+      let y1 = document.createTextNode(data.ingredients[x1])
+      z1.appendChild(y1)
+      ingredList.appendChild(z1)
+    }
 
     // Instructions
     let instructLength = data.instructions.length
-    for (let x = 0; x < instructLength; x++){
-      let z = document.createElement("LI")
-      let y = document.createTextNode(data.instructions[x])
-      z.appendChild(y)
-      instructList.appendChild(z)
+    for (let x2 = 0; x2 < instructLength; x2++){
+      let z2 = document.createElement("LI")
+      let y2 = document.createTextNode(data.instructions[x2])
+      z2.appendChild(y2)
+      instructList.appendChild(z2)
     }
 }
 
