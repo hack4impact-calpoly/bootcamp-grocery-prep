5ccc69f05aeaec14c595d2052d30376979daec52
Emily Gavrilenko <egavrile@calpoly.edu>
2020-11-30 12:25:30 -0800

    Complete Milestone 4

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
- 
+
+const mongoose = require('mongoose')
+const uri = "mongodb+srv://admin:AtlasPassword1@cluster0.vmmxb.mongodb.net/Hack4Impact?retryWrites=true&w=majority";
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Successfully connected to mongodb")
+});
+const recipeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  picture: String,
+  instructions: [String],
+  servings: Number,
+  ratings: [Number]
+}, {collection : "recipes"})
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
  
-app.listen(3000, () =>
-    console.log('Example app listening on port 3000!'),
+app.listen(8000, () =>
+    console.log('Example app listening on port 8000!'),
 );
 
 app.get("/", (request, response) => {
@@ -16,16 +34,32 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/recipe", (request, response) => {
-    response.send("List of recipes coming right up");
+    console.log("List of recipes coming right up");
+    Recipe.find({},  "title", function (err, recipes) {
+        if (err) return handleError(err);
+        // Prints "Space Ghost is a talk show host".
+        console.log(recipes)
+        response.send(recipes);
+      });
 });
 
 app.get("/api/recipe/random", (request, response) => {
-    response.send("Random recipe coming right up!");
+    let recipes = ["Breakfast Burrito", "Cheerios", "Avocado Toast", "Pizza", "Salmon", "Salad"]
+    let index = Math.floor(Math.random() * Math.floor(6));
+    Recipe.findOne({ 'title': recipes[index] },  function (err, recipe) {
+        if (err) return handleError(err);
+        console.log('%s Recipe coming right up', recipe.title);
+        response.send(recipe);
+    });
 });
   
 app.get("/api/recipe/:name", (request, response) => {
     let name = request.params.name
-    response.send("Request for recipe " + name + " coming right up");
+    Recipe.findOne({ 'title': name },  function (err, recipe) {
+        if (err) return handleError(err);
+        console.log('%s Recipe coming right up', recipe.title);
+        response.send(recipe);
+    });
 });
 
 app.get("/api/cart", (request, response) => {
@@ -42,7 +76,17 @@ app.post("/api/cart", (request, response) => {
 
 // Need id and rating
 app.post("/api/rating", (request, response) => {
+    console.log(request.body)
     let id = request.body.id
     let rating = request.body.rating
-    response.send("Adding a " + rating + " star rating for your recipe with id " + id);
+    Recipe.findOne({ 'title': id },  function (err, recipe) {
+        if (err) return handleError(err);
+        console.log('Adding %s star rating to %s Recipe', rating, recipe.title);
+        let ratings = recipe.ratings
+        ratings.push(rating)
+        recipe.ratings = ratings
+        recipe.save()
+        console.log(recipe);
+    });
+    response.send("Added a " + rating + " star rating for your recipe with id " + id);
 });
