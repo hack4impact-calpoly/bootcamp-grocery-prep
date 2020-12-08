3da150de891f54c0052dabd9bde6c09c460ff7a7
Cole <ctperry0301@gmail.com>
2020-12-08 15:36:16 -0600

    Milestone 3 now serves static files

--- GroceryPrep/backend/index.js
+++ GroceryPrep/backend/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../public'));
 
 app.get('/', (req, res) => {
   res.status(200)
