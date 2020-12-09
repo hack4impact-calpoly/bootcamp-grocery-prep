7debe68488170d889913a46fec2dc78e6c05a034
Vee-Guz <vero.guzman48612@gmail.com>
2020-12-08 17:00:00 -0800

    Milestone 3 endpoints

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,30 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-app.use(express.static('public'))
+
+app.get('/api/recipe', (req, res) => {
+  console.log("list of recipes requested")
+  res.send("list of recipes requested")
+})
+
+app.get('/api/recipe/:name', (req, res) => {
+  const name = req.params.name
+
+  console.log("instructions for " +  name + " requested")
+  res.send("instructions for " + name +  " requested")
+})
+
+app.post('/api/rating', (req, res) => {
+  const id = req.body.id
+  const rating = req.body.rating
+  
+  console.log("rating of " + rating + " received for recipe " + id)
+  res.send("rating of " + rating + " received for recipe " + id)
+
+  //console.log(req.body.id, req.body.rating)
+  //res.send("okay")
+})
+
+//app.use(express.static('public'))
 
 app.listen(3000)
