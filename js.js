53e121b4ce11d504c6844494bc9f89fb83749f09
James R <Jamesliamr@outlook.com>
2020-12-08 16:29:32 -0800

    Finished serving button implementation

--- js.js
+++ js.js
@@ -1,10 +1,42 @@
-alert("HELLLLO")
-// const button = document.getElementsByClassName("serving_button").addEventListener("serving_button", console.log("Button!!"))
-const buttonContainer = document.getElementsByClassName('serving_button');
+document.addEventListener('click', event  => {
 
-Array.from(buttonContainer).forEach(b => {
-  b.addEventListener("click", function() {
-    console.log("clicked");
-  });
-});
-console.log("test");
+  if(event.target.id === 'serving_button_minus')
+  {
+    console.log('click minus')
+    // Getting the current serving number
+    let serving_element = document.getElementsByClassName('current_serving')[0]
+    // if its at one then don't update
+    if(serving_element.innerHTML - 1 < 1)
+    {
+      serving_element.innerHTML = 1
+    }
+    else
+    {
+      // Since minus was hit subtract 1 from servings 
+      serving_element.innerHTML -= 1
+      // Grab all the numbers in the recipe
+      let counts = document.getElementsByClassName('num')
+      // Implement updating the recipe here
+      for(i = 0; i < counts.length; i++){
+        counts[i].innerHTML = parseFloat(counts[i].innerHTML) - parseFloat(counts[i].innerHTML)/(parseFloat(serving_element.innerHTML)+1)
+        counts[i].innerHTML = Number.parseFloat(counts[i].innerHTML).toFixed(1)
+      }
+    }
+  }
+  else if(event.target.id === 'serving_button_plus')
+  {
+    console.log('click plus')
+    // Getting the current serving number
+    let serving_element = document.getElementsByClassName('current_serving')[0]  
+    // Since plus was hit add 1 to servings   
+    serving_element.innerHTML = parseInt(serving_element.innerHTML) + parseInt(1)    
+    // Grab all the numbers in the recipe
+    let counts = document.getElementsByClassName('num')
+    // Implement updating the recipe here
+    for(i = 0; i < counts.length; i++){
+      // Update recipe
+      counts[i].innerHTML = parseFloat(counts[i].innerHTML) + (parseFloat(counts[i].innerHTML)/(parseFloat(serving_element.innerHTML)-1))
+      counts[i].innerHTML = Number.parseFloat(counts[i].innerHTML).toFixed(1)
+    }
+  }
+})
