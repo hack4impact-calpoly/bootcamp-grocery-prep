c16f15a72551ebab6d26ea86123f94ac465e5e7b
ghuarng <gary0928@gmail.com>
2020-12-12 13:52:06 -0800

    Add Cart functionality to App -- no recipes yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,40 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header.js'
+import Cart   from './components/Cart.js'
 import About  from './pages/About.js'
 
 class App extends React.Component {
-	
+	constructor(props) {
+		super(props);
+		this.state = { cart: [] };
+	}
+
+	updateCart = (items) => {
+		const cart = this.state.cart;
+		for (const item in items) {
+			if(cart[item] === undefined) {
+				cart[item] = +(items[item]);
+			}
+			else{
+				cart[item] += +(items[item]);
+			}
+		}
+
+		this.setState({cart: cart});
+	}
+
+	emptyCart = () => this.setState({ cart: [] });
+
 	render() {
 		return (
 			<BrowserRouter>
 				<Header />
+				<Route exact path='/'>
+					<main id='browse'>
+						<Cart cart={this.state.cart} emptyCart={this.emptyCart} />
+					</main>
+				</Route>
 				<Route exact path='/about'>
 					<About />
 				</Route>
