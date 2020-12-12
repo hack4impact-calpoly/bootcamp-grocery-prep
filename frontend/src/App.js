b95c3924abf0cc56d553996b25c3ad8a9550c6c8
ghuarng <gary0928@gmail.com>
2020-12-12 13:40:29 -0800

    Clear default App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import './App.css';
 import Header from './components/Header.js'
 import About  from './pages/About.js'
 
