19df03a49ae77b7a492adc3ece480003ee830caa
Ben McMann <bbmcmann@volosys.com>
2020-12-16 15:06:32 -0700

    set up hello world stataic file serving

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+
+app.use(express.static("./../public"))
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world!')
+// })
 
 app.listen(3000)
