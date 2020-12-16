0fb9d25cad86afb937e8c6ac7dda6ee95609ba6f
sleepyStick <itsirisho@gmail.com>
2020-12-15 20:33:21 -0800

    milestone 4

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,50 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
 const mongoose = require('mongoose')
+const url = "mongodb+srv://stick:smolstick@recipes.dodo5.mongodb.net/sticksCafe?retryWrites=true&w=majority"
+const Recipe = require('./models/recipe')
+
 
 app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use("api/recipe/random", express.static("../recipe/random.html"))
-app.use('/css', express.static('css'))
+//app.use(express.static('public'))
+//app.use("api/recipe/random", express.static("../recipe/random.html"))
+//app.use('/css', express.static('css'))
+app.use(express.static('html'))
 
-mongoose.connect("mongodb+srv://stick:sleepy.stick@recipes.mongodb.net/", {
+mongoose.connect(url, {
     useNewUrlParser: true,
     newUnifiedTopolody: true,
     useFindAndModify: false,
     useCreateIndex: true
 }).then(()=> console.log('Connected to MongoDB'))
 
-app.get('/api/recipe', (req, res) => {
-    console.log("list of recipes requested")
+//get all recipes
+const getRecipes = async () => {
+    return await Recipe.find({})
+}
+
+//get one recipe
+const getRecipe = async (name) => {
+    return await Recipe.findOne({'title': name})
+}
+
+const postRating = async (rating, title) => {
+    Recipe.findOne({'title': title}, function (err, recipe) {
+        if (err) 
+            return handleError(err);
+        recipe.ratings.push(Number(rating))
+        recipe.save()
+    });
+}
+
+app.get('/api/recipe', async (req, res) => {
     res.status(200)
-    res.send("list of recipes requested")
+
+    let recipes = await getRecipes()
+
+    res.json(recipes)
 })
 
 //app.get('/api/recipe/random', (req, res) => {
@@ -28,21 +54,34 @@ app.get('/api/recipe', (req, res) => {
     //res.send("random recipe requested")
 //})
 
-app.get('/api/recipe/:name', (req, res) => {
+app.get('/api/recipe/:name', async (req, res) => {
     const name = req.params.name
 
-    console.log('instructions for ' + name + ' requested')
+    let recipe
+
+    recipe = await getRecipe(name)
+
+    //console.log('instructions for ' + name + ' requested')
     res.status(200)
-    res.send("instructions for " + name + " requested")
+    res.json(recipe)
 })
 
-app.post('/api/rating', (req, res) => {
-    const id = req.body.id
-    const rating = req.body.rating
-    console.log("rating of " + req.body.rating + "recieved for recipe with id " + req.body.id)
+app.post('/api/rating', async (req, res) => {
+    const title = req.body.title
+    const rating = req.body.ratings
+    if (rating < 1 || rating > 5) {
+        throw 'Invaid Rating'
+    }
+    await postRating(rating, title);
+    //console.log("rating of " + req.body.rating + " recieved for recipe with id " + req.body.id)
     res.status(200)
-    res.send("rating of " + rating + "recieved for recipe with id " + id)
+    res.send("rating of " + rating + " recieved for recipe with id " + title)
 })
 
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send(200)
+})
 
 app.listen(3000)
+console.log('Serving running on port 3000')
