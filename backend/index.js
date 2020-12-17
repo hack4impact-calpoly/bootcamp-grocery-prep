020379406801a935f898da44d8e9fcb1f673840a
Ben McMann <bbmcmann@volosys.com>
2020-12-17 14:33:35 -0700

    added endpoints for get requests

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const app = express()
 
 
 //app.use(express.static("./../public"))
+//app.use(express.static("./../"))
 
-app.use(express.static("./.."))
+//app.use(express.static("./.."))
+
 // app.get('/', (req, res) => {
 //   res.send('Hello world!')
 // })
@@ -13,5 +15,16 @@ app.use(express.static("./.."))
 //   let page = req.params.page
 //   res.send(page)
 // })
+
+
+app.get("/api/recipe", (req,res) => {
+  res.send("list of recipes requested")
+})
+
+
+app.get("/api/recipe/:name", (req,res) =>{
+  let name = req.params.name
+  res.send(`instructions for ${name} requested`)
+})
 
 app.listen(3000)
