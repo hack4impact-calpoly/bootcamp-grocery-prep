1bb21b24c2a91a941855e4393b6fed0abe719520
Ben McMann <bbmcmann@volosys.com>
2020-12-17 13:14:45 -0700

    tested more examples of using express

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 
 
-app.use(express.static("./../public"))
+//app.use(express.static("./../public"))
 
-
+app.use(express.static("./.."))
 // app.get('/', (req, res) => {
 //   res.send('Hello world!')
+// })
+
+// app.get('/hola/:page', (req, res) => {
+//   let page = req.params.page
+//   res.send(page)
 // })
 
 app.listen(3000)
