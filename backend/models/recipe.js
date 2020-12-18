57bc2c5fa58cbd0e78d5dc60496d3e61167ea7f6
Tessa Tapmongkol <ttapmong@calpoly.edu>
2020-12-18 12:18:40 -0800

    modified Schema

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    desc: {
-        type: String,
-        required: true
-    },
+    title: String,
+    desc: String,
     pictureURL: String,
     ratings: [{
         type: Number,
