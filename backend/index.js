1ab74f4d13a859df37360f5f7e3f7d2f12b6ff05
Ben McMann <bbmcmann@volosys.com>
2020-12-19 15:02:59 -0700

    added status codes to endpoints

--- backend/index.js
+++ backend/index.js
@@ -19,21 +19,24 @@ app.use(bodyParser.json())
 
 
 app.get("/api/recipe", (req,res) => {
+  res.status(200)
   res.send("list of recipes requested")
 })
 
 
 app.get("/api/recipe/:name", (req,res) =>{
+  res.status(200)
   let name = req.params.name
   res.send(`instructions for ${name} requested`)
 })
 
 app.post("/api/rating", (req, res) =>{
+  res.status(200)
   let rating = req.body.rating
   let id = req.body.id
   res.send(`rating of ${rating} for recipe ${id}`)
 })
 
-app.use(express.static("./../public")) //serves index, but cannot link index to site
+app.use(express.static("./../public")) //serves index/html which leads to main site
 
 app.listen(3000)
