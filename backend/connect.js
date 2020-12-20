21f5cace7c5a2014643f42c6b77d72a1b5336258
Ben McMann <bbmcmann@volosys.com>
2020-12-20 16:29:52 -0700

    Successfully added data to test cluster

--- backend/connect.js
+++ backend/connect.js
@@ -3,10 +3,30 @@ const { MongoClient } = require("mongodb");
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = "mongodb+srv://bmcmann:H3ll0W0rld@bootcampc1.shnyh.mongodb.net/database?retryWrites=true&w=majority"
 const client = new MongoClient(url);
+
+const dbName = "database"
 async function run() {
     try {
         await client.connect();
         console.log("Connected correctly to server");
+
+        const db = client.db(dbName)
+
+        const col = db.collection("people")
+
+        let personDocument = {
+
+            "data": "Poggers, this is data #3"
+
+        }
+
+        const p = await col.insertOne(personDocument)
+
+        const myDoc = await col.findOne()
+
+        console.log(myDoc)
+    
+
     } catch (err) {
         console.log(err.stack);
     }
