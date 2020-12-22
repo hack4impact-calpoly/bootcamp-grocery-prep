6383c2f688159f54cecb10d1f251855dc8257b98
sophsuan <47379998+sophsuan@users.noreply.github.com>
2020-12-22 14:45:29 -0800

    Milestone 3 // added status and error check

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,33 @@ const bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world! in index.js');
-})
 
 app.get('/api/recipe', (req, res) => {
+    res.status(200);
     res.send('list of recipes');
 })
 
 app.get('/api/recipe/:name', (req, res) => {
-    let name = req.params.name.substring(1);
+    const name = req.params.name;
+
+    res.status(200);
     res.send(`instruction for ${name} was requested`);
 })
 
 app.post('/api/rating', (req, res) => {
     const rating = req.body.rating;
     const id = req.body.id;
+
+    if (typeof rating === 'undefined' || rating.length === 0) {
+        res.status(400);
+        res.send('error: no rating:(');
+    }
+    if (typeof id === "undefined" || id.length === 0) {
+        res.status(400);
+        res.send('error: no id:(');
+    }
+
+    res.status(200);
     res.send(`rating of ${rating} for recipe ${id}`);
 })
 
