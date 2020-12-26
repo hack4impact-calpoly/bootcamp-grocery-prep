8de93671d99a7aea1432c912cb057c8f93184aec
Ben McMann <bbmcmann@volosys.com>
2020-12-25 17:00:42 -0700

    post rating endpoint correctly updates documents in database

--- backend/api/recipes.js
+++ backend/api/recipes.js
@@ -13,12 +13,21 @@ const getOneRecipe = async (name) => {
     return Recipe.find({title: name})
 }
 
+const updateRating = async (id, rating) => {
+    let doc = await Recipe.findById(id)
+    let rateArray = doc.ratings
+    rateArray.push(rating)
+    // console.log(rateArray)
+    // console.log("********************************")
+    // console.log(rating)
+    return Recipe.updateOne({_id: id}, {ratings: rateArray})
+}
 
 router.get("/recipe", async (req,res) => {
     res.status(200)
     res.send("All recipes requested")
     recipes = await getRecipe()
-    console.log(recipes)
+    // console.log(recipes)
 })
   
   
@@ -27,14 +36,17 @@ router.get("/recipe/:name", async (req,res) =>{
     let name = req.params.name
     recipe = await getOneRecipe(name)
     res.send(`instructions for ${name} requested`)
-    console.log("ONE RECIPE____________________________")
-    console.log(recipe)
+    // console.log("ONE RECIPE____________________________")
+    // console.log(recipe)
 })
   
-router.post("/rating", (req, res) =>{
+router.post("/rating", async (req, res) =>{
     res.status(200)
     let rating = req.body.rating
     let id = req.body.id
+
+    const updatedDoc = await updateRating(id, rating)
+    //res.json(updatedDoc)
     res.send(`rating of ${rating} for recipe ${id}`)
 })
 
