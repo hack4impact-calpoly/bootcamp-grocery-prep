61ad6ee08f7e52e7f249c534e6ebc87c0d45b5f6
Ben McMann <bbmcmann@volosys.com>
2020-12-28 18:17:33 -0700

    fetching data with componentMounted finally works

--- frontend/src/Recipe.js
+++ frontend/src/Recipe.js
@@ -1,12 +1,43 @@
 import './Recipe.css';
+import React from 'react';
 
-function Recipe() {
-  return (
-    <div>
-      
-      <p>This is the recipe cpmponent</p>
-    </div>
-  );
+
+class Recipe extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount(){
+        fetch("http://localhost:3001/api/recipe/Milk", {
+            method: "GET"
+        })
+            .then(res => res.json())
+            .then(data => this.setState(...data));
+    }
+
+    render(){
+
+        return(
+            <div>
+                <p>This is the recipe cpmponent: {this.state.title}</p>
+            </div>
+        );
+
+    }
+
+    
+
 }
+
+// function Recipe() {
+//   return (
+//     <div>
+      
+//       <p>This is the recipe cpmponent</p>
+//     </div>
+//   );
+// }
 
 export default Recipe;
