341a2b915125da252afb863062a0a5a6e6de3f79
Divya Satrawada <dsatrawada@gmail.com>
2021-01-01 15:53:01 -0800

    Add milestone 4

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const mongoose = require('mongoose')
+
+const Recipe = require('./models/recipeSchema')
+
+mongoose.connect("mongodb+srv://dsatrawada:C63XHEbpYUvufUbr@cluster0.ycmwa.mongodb.net/recipes?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => console.log('Connected to MongoDB'))
+
 app.use('/', express.static('../'))
 
 app.use(bodyParser.json())
@@ -12,43 +23,56 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/api/recipe', (req, res) => {
-    console.log(`list of recipes requested`)
+const getRecipes = async () => {
+    return Recipe.find({})
+}
+
+const getRecipe = async (name) => {
+    return Recipe.find({"title": name})
+}
+
+const postRating = async (title, rating) => {
+    Recipe.findOne({"title": title}, function (err, recipe) {
+        recipe.ratings.push(Number(rating))
+        recipe.save()
+    })
+}
+
+app.get('/api/recipe', async (req, res) => {
+    let recipes = await getRecipes()
+    res.send(recipes)
     res.status(200)
-    res.send(`Oatmeal, Peanut Butter and Jelly Sandwich, Macaroni and Cheese, Chocolate Chip Cookies`)
 })
 
-app.get('/api/recipe/:name', (req, res) => {
+app.get('/api/recipe/:name', async (req, res) => {
     const recipeName = req.params.name
 
     if (typeof recipeName === undefined || recipeName.length === 0){
         res.status(400)
         res.send(`Error: NO RECIPE NAME`)
     }
-
-    console.log(`instructions for ${recipeName} requested`)
+    let recipe = await getRecipe(recipeName)
+    res.send(recipe)
     res.status(200)
-    res.send(`${recipeName}`)
 })
 
-app.post('/api/rating', (req, res) => {
+app.post('/api/rating', async (req, res) => {
     const rating = req.body.rating
-    const id = req.body.id
+    const name = req.body.title
 
     if (typeof rating === undefined || rating.length === 0){
         res.status(400)
         res.send(`Error: NO RATING`)
     }
 
-    if (typeof id === undefined || id.length === 0){
+    if (typeof name === undefined || name.length === 0){
         res.status(400)
-        res.send(`Error: NO ID`)
+        res.send(`Error: NO NAME`)
     }
 
-    console.log(req.body.rating, req.body.id)
-    console.log(`rating of ${rating} recieved for recipe ${id}`)
+    await postRating(name, rating)
+    res.send('Rating of ' + rating + ' recieved for ' + name)
     res.status(200)
-    res.send(`${rating} ${id}`)
 })
 
 app.listen(3000)
